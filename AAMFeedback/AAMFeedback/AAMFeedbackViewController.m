0ea4281eee73e7a5f052ee92566286e132803f99
Axel Ros Campaña <actilot@gmail.com>
2013-06-03 13:55:42 +0800

    Removed UIDevice Hardware

--- AAMFeedback/AAMFeedback/AAMFeedbackViewController.m
+++ AAMFeedback/AAMFeedback/AAMFeedbackViewController.m
@@ -8,7 +8,6 @@
 
 #import "AAMFeedbackViewController.h"
 #import "AAMFeedbackTopicsViewController.h"
-#import "UIDeviceHardware.h"
 
 @interface AAMFeedbackViewController (
 private)
@@ -48,12 +47,12 @@ private)
     self = [super initWithStyle:UITableViewStyleGrouped];
     if (self) {
         self.topics = [[[NSArray alloc] initWithObjects:
-                                            NSLocalizedStringFromTable(@"AAMFeedbackTopicsQuestion", @"AAMLocalizable", nil),
-                                            NSLocalizedStringFromTable(@"AAMFeedbackTopicsRequest", @"AAMLocalizable", nil),
-                                            NSLocalizedStringFromTable(@"AAMFeedbackTopicsBugReport", @"AAMLocalizable", nil),
-                                            NSLocalizedStringFromTable(@"AAMFeedbackTopicsMedia", @"AAMLocalizable", nil),
-                                            NSLocalizedStringFromTable(@"AAMFeedbackTopicsBusiness", @"AAMLocalizable", nil),
-                                            NSLocalizedStringFromTable(@"AAMFeedbackTopicsOther", @"AAMLocalizable", nil), nil] autorelease];
+                                            NSLocalizedStringFromTable(@"Question", @"AAMLocalizable", nil),
+                                            NSLocalizedStringFromTable(@"Request", @"AAMLocalizable", nil),
+                                            NSLocalizedStringFromTable(@"BugReport", @"AAMLocalizable", nil),
+                                            NSLocalizedStringFromTable(@"Media", @"AAMLocalizable", nil),
+                                            NSLocalizedStringFromTable(@"Business", @"AAMLocalizable", nil),
+                                            NSLocalizedStringFromTable(@"Other", @"AAMLocalizable", nil), nil] autorelease];
 
         self.topicsToSend = [[self.topics copy] autorelease];
     }
@@ -370,7 +369,38 @@ private)
 
 
 - (NSString *)_platformString {
-    return [UIDeviceHardware platformString];
+  NSString *platform = [self _platform];
+  NSLog(@"%@",platform);
+  if ([platform isEqualToString:@"iPhone1,1"]) return @"iPhone 1G";
+  if ([platform isEqualToString:@"iPhone1,2"]) return @"iPhone 3G";
+  if ([platform isEqualToString:@"iPhone2,1"]) return @"iPhone 3GS";
+  if ([platform isEqualToString:@"iPhone3,1"]) return @"iPhone 4";
+  if ([platform isEqualToString:@"iPhone3,3"]) return @"iPhone 4";
+  if ([platform isEqualToString:@"iPhone4,1"]) return @"iPhone 4S";
+  if ([platform isEqualToString:@"iPhone5,1"]) return @"iPhone 5";
+  if ([platform isEqualToString:@"iPhone5,2"]) return @"iPhone 5";
+  if ([platform isEqualToString:@"iPhone5,3"]) return @"iPhone 5";
+  if ([platform isEqualToString:@"iPod1,1"])   return @"iPod Touch 1G";
+  if ([platform isEqualToString:@"iPod2,1"])   return @"iPod Touch 2G";
+  if ([platform isEqualToString:@"iPod3,1"])   return @"iPod Touch 3G";
+  if ([platform isEqualToString:@"iPod4,1"])   return @"iPod Touch 4G";
+  if ([platform isEqualToString:@"iPod5,1"])   return @"iPod Touch 5G";
+  if ([platform isEqualToString:@"iPad1,1"])   return @"iPad";
+  if ([platform isEqualToString:@"iPad2,1"])   return @"iPad 2 (WiFi)";
+  if ([platform isEqualToString:@"iPad2,2"])   return @"iPad 2 (GSM)";
+  if ([platform isEqualToString:@"iPad2,3"])   return @"iPad 2 (CDMA)";
+  if ([platform isEqualToString:@"iPad2,5"])   return @"iPad Mini (WiFi)";
+  if ([platform isEqualToString:@"iPad2,6"])   return @"iPad Mini (GSM)";
+  if ([platform isEqualToString:@"iPad2,7"])   return @"iPad Mini (GSM+CDMA)";
+  if ([platform isEqualToString:@"iPad3,1"])   return @"iPad 3 (WiFi)";
+  if ([platform isEqualToString:@"iPad3,2"])   return @"iPad 3 (GSM)";
+  if ([platform isEqualToString:@"iPad3,3"])   return @"iPad 3 (CDMA)";
+  if ([platform isEqualToString:@"iPad4,1"])   return @"iPad 4 (WiFi)";
+  if ([platform isEqualToString:@"iPad4,2"])   return @"iPad 4 (GSM)";
+  if ([platform isEqualToString:@"iPad4,3"])   return @"iPad 4 (CDMA)";
+  if ([platform isEqualToString:@"i386"])      return @"iPhone Simulator";
+  if ([platform isEqualToString:@"x86_64"])    return @"iPhone Simulator";
+  return platform;
 }
 
 @end
